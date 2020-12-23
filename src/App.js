3f56ca59d5eb7039e88d98ee94b7180ab342152b
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-23 18:41:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 //Import Components
-import {NavBar} from './assests/components/NavBar/NavBar';
+import NavBar from './assests/components/NavBar/NavBar';
 
 function App () {
   return (

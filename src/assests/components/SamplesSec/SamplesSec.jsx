e8f30f475989c15cf9963165385ccd4b69767415
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-24 16:09:13 +0500

    Update SamplesSec.jsx

--- src/assests/components/SamplesSec/SamplesSec.jsx
+++ src/assests/components/SamplesSec/SamplesSec.jsx
@@ -20,86 +20,74 @@ export const SamplesSec = () => {
                                     <CardMedia
                                         component="img"
                                         alt="Contemplative Reptile"
-                                        height="140"
+                                        height="200"
                                         image={Chatbot}
                                         title="Contemplative Reptile"
                                     />
                                     <CardContent>
                                         <Typography gutterBottom variant="h5" component="h2">
-                                            Lizard
+                                            Scripted Chatbot
                                         </Typography>
                                         <Typography variant="body2" color="textSecondary" component="p">
-                                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                            across all continents except Antarctica
+                                            Conversation design guidelines such as chatbot personality, flowcharts, and scripts helped me convert complex mattress fitting data into simple dialogue for a scripted chatbot that guides customers in choosing the right mattress for their weight range.
                                         </Typography>
                                     </CardContent>
                                 </CardActionArea>
                                 <CardActions>
                                     <Button size="small" color="primary">
-                                        Share
-                                    </Button>
-                                    <Button size="small" color="primary">
-                                        Learn More
+                                        Case study  
                                     </Button>
                                 </CardActions>
                             </Card>
                         </Grid>
                         <Grid item xs={12} md={4}>
-                        <Card>
+                            <Card>
                                 <CardActionArea>
                                     <CardMedia
                                         component="img"
                                         alt="Contemplative Reptile"
-                                        height="140"
+                                        height="200"
                                         image={Speedpix}
                                         title="Contemplative Reptile"
                                     />
                                     <CardContent>
                                         <Typography gutterBottom variant="h5" component="h2">
-                                            Lizard
+                                            User Onboarding
                                         </Typography>
                                         <Typography variant="body2" color="textSecondary" component="p">
-                                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                            across all continents except Antarctica
+                                            Here is the first screen in an onboarding process I wrote for a SaaS product. To create a smooth introduction to the product and improve conversion rates, I used reassuring details and informative labels to clear possible uncertainties and reduce ambiguity.
                                         </Typography>
                                     </CardContent>
                                 </CardActionArea>
                                 <CardActions>
                                     <Button size="small" color="primary">
-                                        Share
-                                    </Button>
-                                    <Button size="small" color="primary">
-                                        Learn More
+                                        See more
                                     </Button>
                                 </CardActions>
                             </Card>
                         </Grid>
                         <Grid item xs={12} md={4}>
-                        <Card>
+                            <Card>
                                 <CardActionArea>
                                     <CardMedia
                                         component="img"
                                         alt="Contemplative Reptile"
-                                        height="140"
+                                        height="200"
                                         image={StartScale}
                                         title="Contemplative Reptile"
                                     />
                                     <CardContent>
                                         <Typography gutterBottom variant="h5" component="h2">
-                                            Lizard
+                                            Product Development
                                         </Typography>
                                         <Typography variant="body2" color="textSecondary" component="p">
-                                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                            across all continents except Antarctica
+                                            As the sole UX writer on a product team building the 'Uber of marketing', I wrote and designed web copy, microcopy, notifications, forms, FAQs, sales video script, automated emails, signup page, login page, pricing page, checkout page, and more.
                                         </Typography>
                                     </CardContent>
                                 </CardActionArea>
                                 <CardActions>
                                     <Button size="small" color="primary">
-                                        Share
-                                    </Button>
-                                    <Button size="small" color="primary">
-                                        Learn More
+                                        Case study coming in 2020   
                                     </Button>
                                 </CardActions>
                             </Card>

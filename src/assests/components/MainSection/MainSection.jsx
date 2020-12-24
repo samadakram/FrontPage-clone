1ef14e353e805974f8fb1a5cad054169bb028d67
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-24 12:42:56 +0500

    Update MainSection.jsx

--- src/assests/components/MainSection/MainSection.jsx
+++ src/assests/components/MainSection/MainSection.jsx
@@ -6,13 +6,13 @@ export const MainSection = () => {
     return (
         <div>
             <Grid container>
-                <Grid item md={4}>
+                <Grid item md={5}>
                     <h2>CORTEX</h2>
                     <h2>COPYWRITER</h2>
                     <p>Enhance your communications with psychology-based copywriting and UX writing</p>
                     <button>Send message</button>
                 </Grid>
-                <Grid item md={8}>
+                <Grid item md={7}>
                     <img src={Factory} alt="factory"/>
                 </Grid>
             </Grid>

b933400fe8d24faf2440000a12fbc07a5255a332
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-24 17:18:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NavBar from './assests/components/NavBar/NavBar';
 import {MainSection} from './assests/components/MainSection/MainSection';
 import {ServicesSection} from './assests/components/ServicesSection/ServicesSection';
 import {SamplesSec} from './assests/components/SamplesSec/SamplesSec';
+import {Reviews} from './assests/components/Reviews/Reviews';
 
 function App () {
   return (
@@ -14,6 +15,7 @@ function App () {
       <MainSection />
       <ServicesSection />
       <SamplesSec />
+      <Reviews />
     </div>
   )
 }

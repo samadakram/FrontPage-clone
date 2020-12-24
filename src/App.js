c39f37e2947837425b579acec5f29f1b56bfd824
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-24 13:15:27 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import styles from './App.module.css';
 //Import Components
 import NavBar from './assests/components/NavBar/NavBar';
 import {MainSection} from './assests/components/MainSection/MainSection';
+import {ServicesSection} from './assests/components/ServicesSection/ServicesSection';
 
 function App () {
   return (
     <div className={styles.mainContainer}>
       <NavBar />
       <MainSection />
+      <ServicesSection />
     </div>
   )
 }

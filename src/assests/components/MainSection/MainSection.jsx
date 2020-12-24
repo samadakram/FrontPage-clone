7728a400ec5ecd83b3748902925a86ad236e109f
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-24 12:47:40 +0500

    Update MainSection.jsx

--- src/assests/components/MainSection/MainSection.jsx
+++ src/assests/components/MainSection/MainSection.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import Factory from '../../Images/ImageEmpty.svg';
+import styles from './MainSection.module.css';
 
 export const MainSection = () => {
     return (
         <div>
             <Grid container>
-                <Grid item md={5}>
+                <Grid item md={5} className={styles.gridText}>
                     <h2>CORTEX</h2>
                     <h2>COPYWRITER</h2>
                     <p>Enhance your communications with psychology-based copywriting and UX writing</p>
                     <button>Send message</button>
                 </Grid>
-                <Grid item md={7}>
+                <Grid item md={7} className={styles.gridImage}>
                     <img src={Factory} alt="factory"/>
                 </Grid>
             </Grid>

7a3a43e3e641a0ae650b670d628ed78db917b2d1
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-24 18:31:38 +0500

    Update Email.jsx

--- src/assests/components/Email/Email.jsx
+++ src/assests/components/Email/Email.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Grid, Typography } from '@material-ui/core';
 
 export const Email = () => {
     return (
         <div>
-            <h1>Emaail Section</h1>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Typography variant="h4">
+                        Pick my brain/cortex and let's get<br/> started on your project
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    
+                </Grid>
+            </Grid>
         </div>
     );
 }

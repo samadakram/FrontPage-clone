9e9f6705fb8a6bf2559cc47d61ac57ce3b4ac378
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-24 17:07:35 +0500

    Update SamplesSec.jsx

--- src/assests/components/SamplesSec/SamplesSec.jsx
+++ src/assests/components/SamplesSec/SamplesSec.jsx
@@ -4,6 +4,11 @@ import { Grid, Card, CardActionArea, CardActions, CardContent, CardMedia, Typogr
 import Chatbot from '../../Images/cardImages/chatbot.jpg';
 import Speedpix from '../../Images/cardImages/speedpix.jpg';
 import StartScale from '../../Images/cardImages/startscale.jpg';
+import Blaze from '../../Images/cardImages/blaze1.jpg';
+import Tara from '../../Images/cardImages/tara2.jpg';
+import PureLearning from '../../Images/cardImages/purelearning3.jpg';
+import AdGroove from '../../Images/cardImages/adgroove4.jpg';
+import Sample from '../../Images/cardImages/sample5.jpg';
 
 export const SamplesSec = () => {
     return (
@@ -36,7 +41,7 @@ export const SamplesSec = () => {
                                 </CardActionArea>
                                 <CardActions>
                                     <Button size="small" color="primary">
-                                        Case study  
+                                        Case study
                                     </Button>
                                 </CardActions>
                             </Card>
@@ -88,7 +93,7 @@ export const SamplesSec = () => {
                                 </CardActionArea>
                                 <CardActions>
                                     <Button size="small" color="primary">
-                                        Case study coming in 2020   
+                                        Case study coming in 2020
                                     </Button>
                                 </CardActions>
                             </Card>
@@ -107,21 +112,21 @@ export const SamplesSec = () => {
                                         component="img"
                                         alt="Contemplative Reptile"
                                         height="200"
-                                        image={Chatbot}
+                                        image={Blaze}
                                         title="Contemplative Reptile"
                                     />
                                     <CardContent>
                                         <Typography gutterBottom variant="h5" component="h2">
-                                            Scripted Chatbot
+                                            Rebranding Campaign
                                         </Typography>
                                         <Typography variant="body2" color="textSecondary" component="p">
-                                            Conversation design guidelines such as chatbot personality, flowcharts, and scripts helped me convert complex mattress fitting data into simple dialogue for a scripted chatbot that guides customers in choosing the right mattress for their weight range.
+                                            I developed a new brand voice and brand identity – content style guide, design system, brand name, logo, tagline, and website – to improve communications and online presence.
                                         </Typography>
                                     </CardContent>
                                 </CardActionArea>
                                 <CardActions>
                                     <Button size="small" color="primary">
-                                        Case study  
+                                        Case study
                                     </Button>
                                 </CardActions>
                             </Card>
@@ -133,15 +138,15 @@ export const SamplesSec = () => {
                                         component="img"
                                         alt="Contemplative Reptile"
                                         height="200"
-                                        image={Speedpix}
+                                        image={Tara}
                                         title="Contemplative Reptile"
                                     />
                                     <CardContent>
                                         <Typography gutterBottom variant="h5" component="h2">
-                                            User Onboarding
+                                            Facebook Ads
                                         </Typography>
                                         <Typography variant="body2" color="textSecondary" component="p">
-                                            Here is the first screen in an onboarding process I wrote for a SaaS product. To create a smooth introduction to the product and improve conversion rates, I used reassuring details and informative labels to clear possible uncertainties and reduce ambiguity.
+                                            I’ve lost count of how many Facebook ads I’ve written. My general strategy for writing ads with limited space is to make sure the copy focuses on a single, specific thought, and shows empathy. Check out some of my recent ads.
                                         </Typography>
                                     </CardContent>
                                 </CardActionArea>
@@ -159,21 +164,21 @@ export const SamplesSec = () => {
                                         component="img"
                                         alt="Contemplative Reptile"
                                         height="200"
-                                        image={StartScale}
+                                        image={PureLearning}
                                         title="Contemplative Reptile"
                                     />
                                     <CardContent>
                                         <Typography gutterBottom variant="h5" component="h2">
-                                            Product Development
+                                            Explainer Video
                                         </Typography>
                                         <Typography variant="body2" color="textSecondary" component="p">
-                                            As the sole UX writer on a product team building the 'Uber of marketing', I wrote and designed web copy, microcopy, notifications, forms, FAQs, sales video script, automated emails, signup page, login page, pricing page, checkout page, and more.
+                                            Here’s a screenshot for a video I wrote, storyboarded, and directed. The aim was to build awareness for an eLearning company. The copy – which inspired the audience to think, feel, and act – helped increase sales by 9.86%.
                                         </Typography>
                                     </CardContent>
                                 </CardActionArea>
                                 <CardActions>
                                     <Button size="small" color="primary">
-                                        Case study coming in 2020   
+                                        See more
                                     </Button>
                                 </CardActions>
                             </Card>
@@ -185,21 +190,21 @@ export const SamplesSec = () => {
                                         component="img"
                                         alt="Contemplative Reptile"
                                         height="200"
-                                        image={Chatbot}
+                                        image={AdGroove}
                                         title="Contemplative Reptile"
                                     />
                                     <CardContent>
                                         <Typography gutterBottom variant="h5" component="h2">
-                                            Scripted Chatbot
+                                            YouTube Ad
                                         </Typography>
                                         <Typography variant="body2" color="textSecondary" component="p">
-                                            Conversation design guidelines such as chatbot personality, flowcharts, and scripts helped me convert complex mattress fitting data into simple dialogue for a scripted chatbot that guides customers in choosing the right mattress for their weight range.
+                                            Here is Groovysaurus Rex – the company mascot in a 3D animated video advertisement which I wrote, storyboarded, and directed. The ad ran for 30 days and yielded 19 conversions, with a 67% view rate.
                                         </Typography>
                                     </CardContent>
                                 </CardActionArea>
                                 <CardActions>
                                     <Button size="small" color="primary">
-                                        Case study  
+                                        See more
                                     </Button>
                                 </CardActions>
                             </Card>
@@ -211,21 +216,21 @@ export const SamplesSec = () => {
                                         component="img"
                                         alt="Contemplative Reptile"
                                         height="200"
-                                        image={Speedpix}
+                                        image={Sample}
                                         title="Contemplative Reptile"
                                     />
                                     <CardContent>
                                         <Typography gutterBottom variant="h5" component="h2">
-                                            User Onboarding
+                                            Pilot Screenplay
                                         </Typography>
                                         <Typography variant="body2" color="textSecondary" component="p">
-                                            Here is the first screen in an onboarding process I wrote for a SaaS product. To create a smooth introduction to the product and improve conversion rates, I used reassuring details and informative labels to clear possible uncertainties and reduce ambiguity.
+                                            Screenwriting is one of my specialties. My award-winning, original, sci-fi, adult animated, pilot screenplay has successfully been pitched to production companies in the USA and Australia.
                                         </Typography>
                                     </CardContent>
                                 </CardActionArea>
                                 <CardActions>
                                     <Button size="small" color="primary">
-                                        See more
+                                        Award here
                                     </Button>
                                 </CardActions>
                             </Card>

d292a2a3cc22bfe2207a741bfc21cece1b81ba15
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-24 17:42:22 +0500

    Update Reviews.jsx

--- src/assests/components/Reviews/Reviews.jsx
+++ src/assests/components/Reviews/Reviews.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+
+// Import Images
+import Image1 from '../../Images/reviewImages/pic1.svg';
+import Image2 from '../../Images/reviewImages/pic2.svg';
+import Image3 from '../../Images/reviewImages/pic3.svg';
+import Image4 from '../../Images/reviewImages/pic4.svg';
+import Image5 from '../../Images/reviewImages/pic5.svg';
+import Image6 from '../../Images/reviewImages/pic6.svg';
 
 export const Reviews = () => {
     return (
         <div>
-            <h1>Reviews</h1>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Grid container>
+                        <Grid item xs={12} md={6}>
+                            <div>
+                                <div>
+                                    <img src={Image1} alt="image 1" />
+                                </div>
+                                <div>
+                                    <h5>Karl Van Lieshout</h5>
+                                    <p>Owner & Operator – Ausbeds</p>
+                                    <h4>"..innovative"<br/>"..seamless UX""<br/>..more online sales"</h4>
+                                </div>
+                            </div>
+                        </Grid>
+                        <Grid item xs={12} md={6}>Div 2</Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12}>Line 2</Grid>
+                <Grid item xs={12}>Line 3</Grid>
+            </Grid>
         </div>
     );
 }

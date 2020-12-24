7df6b0a0b6f6c6f20287451bd6ba02853ffa90b9
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-24 13:40:22 +0500

    Update ServicesSection.jsx

--- src/assests/components/ServicesSection/ServicesSection.jsx
+++ src/assests/components/ServicesSection/ServicesSection.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import styles from './ServicesSection.module.css';
 import { Grid } from '@material-ui/core';
 
+// Import Images
+import UX from '../../Images/servicesImages/services1.svg'; 
+
 export const ServicesSection = () => {
     return (
         <div>
             <Grid container>
                 <Grid item xs={12} className={styles.grid}>
                     <Grid container>
-                        <Grid item md={6}>Image</Grid>
+                        <Grid item md={6}>
+                            <img src={UX} alt="UX writing"/>
+                        </Grid>
                         <Grid item md={6}>TEXT</Grid>
                     </Grid>
                 </Grid>

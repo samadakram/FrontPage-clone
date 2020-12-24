e8fa96c1d471dea7ddbaa7d4a2c81981f0a19346
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-24 14:30:32 +0500

    Update SamplesSec.jsx

--- src/assests/components/SamplesSec/SamplesSec.jsx
+++ src/assests/components/SamplesSec/SamplesSec.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 
 export const SamplesSec = () => {
     return (
         <div>
             <h1>Samples</h1>
+            <Grid container>
+                <Grid item xs={12}>
+                    <h3>Useful UX</h3>
+                </Grid>
+                <Grid item xs={12}>
+                    <Grid container>
+                        <Grid item md={4}>CARD 1</Grid>
+                        <Grid item md={4}>CARD 2</Grid>
+                        <Grid item md={4}>CARD 3</Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         </div>
     );
 }

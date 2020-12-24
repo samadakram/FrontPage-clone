46accdb835bd6cc38377059695ff040d5c6406a2
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-24 14:35:50 +0500

    Update SamplesSec.jsx

--- src/assests/components/SamplesSec/SamplesSec.jsx
+++ src/assests/components/SamplesSec/SamplesSec.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography, Button } from '@material-ui/core';
 
 export const SamplesSec = () => {
     return (
@@ -11,7 +11,36 @@ export const SamplesSec = () => {
                 </Grid>
                 <Grid item xs={12}>
                     <Grid container>
-                        <Grid item xs={12} md={4}>CARD 1</Grid>
+                        <Grid item xs={12} md={4}>
+                            <Card>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        alt="Contemplative Reptile"
+                                        height="140"
+                                        image="/static/images/cards/contemplative-reptile.jpg"
+                                        title="Contemplative Reptile"
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Lizard
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                            across all continents except Antarctica
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <CardActions>
+                                    <Button size="small" color="primary">
+                                        Share
+                                    </Button>
+                                    <Button size="small" color="primary">
+                                        Learn More
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
                         <Grid item xs={12} md={4}>CARD 2</Grid>
                         <Grid item xs={12} md={4}>CARD 3</Grid>
                     </Grid>

3fb728d20e9fea53038ec72e31b2bc20ea89faf6
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-24 18:10:37 +0500

    Update Reviews.jsx

--- src/assests/components/Reviews/Reviews.jsx
+++ src/assests/components/Reviews/Reviews.jsx
@@ -19,20 +19,85 @@ export const Reviews = () => {
                         <Grid item xs={12} md={6}>
                             <div className={styles.reviewDiv}>
                                 <div>
-                                    <img src={Image1} alt="image 1" />
+                                    <img src={Image1} alt="avatar1" className={styles.image}/>
                                 </div>
                                 <div>
                                     <h5>Karl Van Lieshout</h5>
                                     <p>Owner & Operator – Ausbeds</p>
-                                    <h4>"..innovative"<br/>"..seamless UX""<br/>..more online sales"</h4>
+                                    <h4>"..innovative"<br/>"..seamless UX""<br/>"..more online sales"</h4>
                                 </div>
                             </div>
                         </Grid>
-                        <Grid item xs={12} md={6}>Div 2</Grid>
+                        <Grid item xs={12} md={6}>
+                        <div className={styles.reviewDiv}>
+                                <div>
+                                    <img src={Image2} alt="avatar2" className={styles.image}/>
+                                </div>
+                                <div>
+                                    <h5>Rosawin Siri</h5>
+                                    <p>Digital Marketing Manager – 4mation Technologies</p>
+                                    <h4>"..flexible"<br/>"..nailed voice & tone"<br/>"..keep on retainer"</h4>
+                                </div>
+                            </div>
+                        </Grid>
                     </Grid>
                 </Grid>
-                <Grid item xs={12}>Line 2</Grid>
-                <Grid item xs={12}>Line 3</Grid>
+                <Grid item xs={12}>
+                <Grid container>
+                        <Grid item xs={12} md={6}>
+                            <div className={styles.reviewDiv}>
+                                <div>
+                                    <img src={Image3} alt="avatar3" className={styles.image}/>
+                                </div>
+                                <div>
+                                    <h5>Craig Erskine-Smith</h5>
+                                    <p>CEO – Erskine Oral Care</p>
+                                    <h4>"..wonderful ideas"<br/>"..perfect tagline"<br/>"..excellent wireframes"</h4>
+                                </div>
+                            </div>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                        <div className={styles.reviewDiv}>
+                                <div>
+                                    <img src={Image4} alt="avatar4" className={styles.image}/>
+                                </div>
+                                <div>
+                                    <h5>Liam Key</h5>
+                                    <p>Creative Director – Imagination Pioneers Studios</p>
+                                    <h4>"..love his passion"<br/>..heaps of depth"<br/>"..speaks to my brand"</h4>
+                                </div>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                <Grid container>
+                        <Grid item xs={12} md={6}>
+                            <div className={styles.reviewDiv}>
+                                <div>
+                                    <img src={Image5} alt="avatar5" className={styles.image}/>
+                                </div>
+                                <div>
+                                    <h5>Eoin Mullins</h5>
+                                    <p>Director – Corbel Technologies</p>
+                                    <h4>"..beautiful copy"<br/>"..psychology edge"<br/>"..emotional currents"</h4>
+                                </div>
+                            </div>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                        <div className={styles.reviewDiv}>
+                                <div>
+                                    <img src={Image6} alt="avatar6" className={styles.image}/>
+                                </div>
+                                <div>
+                                    <h5>Adrian Rodriguez Soto</h5>
+                                    <p>eCommerce Manager – EUX Agency</p>
+                                    <h4>"..superior intellect"<br/>"..deep psychology"<br/>"..doubled conversions"</h4>
+                                </div>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Grid>
             </Grid>
         </div>
     );

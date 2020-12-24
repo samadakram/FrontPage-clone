b401a2ed4616055e179784a197c60442be8479c8
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-24 13:21:07 +0500

    Update ServicesSection.jsx

--- src/assests/components/ServicesSection/ServicesSection.jsx
+++ src/assests/components/ServicesSection/ServicesSection.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 
 export const ServicesSection = () => {
     return (
         <div>
-            <h1>Services Section</h1>
+            <Grid container>
+                <Grid item xs={12}>UX Design</Grid>
+                <Grid item xs={12}>Content CopyWrititng</Grid>
+                <Grid item xs={12}>Content Writing</Grid>
+            </Grid>
         </div>
     );
 }

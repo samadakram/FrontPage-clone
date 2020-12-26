c0ffbcb9931f91092250d0fd7145a26809a196ee
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-26 11:13:04 +0500

    Update MainSection.jsx

--- src/assests/components/MainSection/MainSection.jsx
+++ src/assests/components/MainSection/MainSection.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid, Typography, Button } from '@material-ui/core';
 import styles from './MainSection.module.css';
 import useWebAnimations from '@wellyshen/use-web-animations';
+import { motion } from 'framer-motion';
 
 // Import Images
 import Factory from '../../Images/ImageEmpty.svg';
@@ -14,8 +15,8 @@ export const MainSection = () => {
 
     const brain = useWebAnimations({
         keyframes: [
-            {transform: 'translateY(-20px)'},
-            {transform: 'translateY(20px)'}
+            { transform: 'translateY(-20px)' },
+            { transform: 'translateY(20px)' }
         ],
         timing: {
             duration: 2000,
@@ -32,12 +33,14 @@ export const MainSection = () => {
                     <Typography variant="h2">CORTEX</Typography>
                     <Typography variant="h2">COPYWRITER</Typography>
                     <Typography variant="body1">Enhance your communications with psychology-based copywriting and UX writing</Typography>
-                    <Button variant="contained" className={styles.button}>Send message</Button>
+                    <motion.span>
+                        <Button variant="contained" className={styles.button}>Send message</Button>
+                    </motion.span>
                 </Grid>
                 <Grid item xs={12} md={7} className={styles.gridImage}>
-                    <img src={Factory} alt="factory" className="factoryImage"/>
-                    <img src={Layer} alt="layer" className={styles.layer}/>
-                    <img ref={brain.ref} src={Brain} alt="Brain" className={styles.brain}/>
+                    <img src={Factory} alt="factory" className="factoryImage" />
+                    <img src={Layer} alt="layer" className={styles.layer} />
+                    <img ref={brain.ref} src={Brain} alt="Brain" className={styles.brain} />
                 </Grid>
             </Grid>
         </div>
